c40ef33cdf74e716be0e3187fba1b274f914d2f9
MaksymTymeichuk <105876357+MaksymTymeichuk@users.noreply.github.com>
2022-08-19 17:42:50 +0300

    game itself

--- script.js
+++ script.js
@@ -37,6 +37,25 @@ var choosenCardsId = [];
     playCards[i].setAttribute("id", i) //встановлюємо id для кожної картки
     playCards[i].addEventListener("click", function () {
       this.style.backgroundImage = `url(${shuffleArray[i].img})`;
+      choosenCards.push(shuffleArray[this.id].name)
+      choosenCardsId.push(this.id)
+      // коли в масива дві картки
+      if(choosenCards.length === 2){
+        console.log("!")
+        setTimeout(function(){//відстрочка виконання функції
+            if(choosenCards[0] === choosenCards[1]){
+                playCards[choosenCardsId[0]].style.visibility = "hidden"
+                playCards[choosenCardsId[1]].style.visibility = "hidden" //приховуємо картки
+            }
+                else{
+                playCards[choosenCardsId[0]].style.backgroundImage = "none"
+                playCards[choosenCardsId[1]].style.backgroundImage = "none"
+            }
+            choosenCardsId = []
+            choosenCards = []
+            
+        }, 500);
+      }
     });
   }
   
